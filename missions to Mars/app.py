668623c11f6ed0e94359fab012a939757dd9ef1a
liambrekke <liam.brekke@gmail.com>
2021-04-03 14:54:52 -0500

    created route to index.

--- missions to Mars/app.py
+++ missions to Mars/app.py
@@ -6,5 +6,13 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+mongo = PyMongo(app, uri = 'mongodb://localhost:27017/mars_app')
+
+# Index Route
+@app.route('/')
+def index():
+    
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars=mars)
+
+

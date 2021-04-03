19d5d6bd4b90be717b97562e1d2c95ec6f921ea1
liambrekke <liam.brekke@gmail.com>
2021-04-03 13:09:17 -0500

    dependencies

---
+++ missions to Mars/scrape_mars.py
@@ -0,0 +1,9 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
+import pandas as pd
+import os
+import time
+import pymongo

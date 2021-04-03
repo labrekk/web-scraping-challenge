2471a147f36aeff37f265d61ccaab6253088114e
liambrekke <liam.brekke@gmail.com>
2021-04-03 13:26:05 -0500

    parsing set up for nasa news

--- missions to Mars/scrape_mars.py
+++ missions to Mars/scrape_mars.py
@@ -1,6 +1,6 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from splinter import Browser
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
 import requests
 import pandas as pd
@@ -17,4 +17,21 @@ def scrape():
     mars_dict = {}
 
     url = 'https://mars.nasa.gov/news/'
+    # visit nasa news
+    browser.visit(url)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+#parse for titles and article teaser text
+    titles = soup.find_all('div', class_='content_title')
+    articles = soup.find_all('div', class_='article_teaser_body')
+
+    article_title = titles[1].text.strip()
+    article_text = articles.text.strip()
+
+    #append to dict
+    mars_dict['article_title'] = article_title
+    mars_dict['article_text'] = article_text
+
     
